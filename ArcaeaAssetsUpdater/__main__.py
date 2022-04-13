b8db5a45f11830566a3573d5f72502bfdd10f545
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-04-14 01:31:42 +0800

    Update Dockerfile

--- ArcaeaAssetsUpdater/__main__.py
+++ ArcaeaAssetsUpdater/__main__.py
@@ -16,4 +16,4 @@ async def _():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app=app, host='0.0.0.0', port=17777)
+    uvicorn.run(app=app, host='0.0.0.0', port=8080)
---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.9.12-slim
+COPY . /app/
+WORKDIR /app
+RUN pip install poetry && poetry install
+EXPOSE 8080
+ENTRYPOINT poetry run python3 ArcaeaAssetsUpdater
